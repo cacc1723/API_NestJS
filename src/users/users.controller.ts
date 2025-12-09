ed5d1b29d77d771f3a76ba7e9fa30f383e9ea7dc
Carlos Cabrera <cabrera.carlos85@outlook.com>
2025-12-09 15:57:53 -0500

    Primer Commit proyecto modelo de APIs Nest JS

---
+++ src/users/users.controller.ts
@@ -0,0 +1,56 @@
+import { Controller, Get, Param, Post, Body, Delete } from '@nestjs/common';
+
+interface User {
+  id: string;
+  name: string;
+  email: string;
+}
+
+@Controller('users')
+export class UsersController {
+  private users: User[] = [
+    { id: '1', name: 'Alice', email: 'alice@example.com' },
+    { id: '2', name: 'Bob', email: 'bob@example.com' },
+    { id: '3', name: 'Charlie', email: 'charlie@example.com' },
+  ];
+
+  @Get()
+  getAllUsers() {
+    return this.users;
+  }
+
+  @Get(':id')
+  FindUsers(@Param('id') id: string) {
+    const user = this.users.find((user) => user.id === id);
+    if (!user) {
+      return {
+        message: 'User not found',
+      };
+  };
+     return user;
+  }
+
+  @Post()
+  createUser(@Body() newUserData: { name: string; email: string }) {
+    const newUser: User = {
+      id: (this.users.length + 1).toString(),
+      name: newUserData.name,
+      email: newUserData.email,
+    };
+    this.users.push(newUser);
+    return newUser;
+  }
+
+  @Delete(':id')
+  deleteUser(@Param('id') id: string) {
+    const userIndex = this.users.findIndex((user) => user.id === id);
+    if (userIndex === -1) {
+      return {
+        message: 'User not found',
+      };
+    }
+    const deletedUser = this.users.splice(userIndex, 1)[0];
+    return { message: 'User deleted successfully', user: deletedUser };
+  }
+
+}
