0442a813c8a55859f2e078e31641c0023cfff75d
Carlos Cabrera <cabrera.carlos85@outlook.com>
2025-12-15 10:12:41 -0500

    Add Users services

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,25 @@
 import { Controller, Get } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { AppService } from './app.service';
+import { UsersService } from './users/users.service';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(
+    private readonly appService: AppService,
+    private readonly usersService: UsersService,
+    private readonly configService: ConfigService,
+  ) {}
 
   @Get()
   getHello(): string {
+    const myVar = this.configService.get<string>('MY_VAR');
+    console.log('MY_VAR:', myVar);
     return this.appService.getHello();
+  }
+
+  @Get('my-test')
+  getMyTest() {
+    return this.usersService.findAll();
   }
 }
