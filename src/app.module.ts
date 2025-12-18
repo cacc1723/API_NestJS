fe6ff03ee0ea9b57f6e8b2dddbab61b9e15b0268
Carlos Cabrera <cabrera.carlos85@outlook.com>
2025-12-18 10:27:41 -0500

    Add Docker PostgreSQL and conf type ORM

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,22 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { UsersModule } from './users/users.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
+    }),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'blog_user',
+      password: 'blog_password',
+      database: 'my_blog_db',
+      autoLoadEntities: true,
+      synchronize: true,
     }),
     UsersModule,
   ],
